3704ca6504ca384841a66de7ef19a5e35c9aa1d2
shuguang101 <1511013670@qq.com>
2020-08-07 18:00:55 +0800

    CTF是啥

--- JSteg.py
+++ JSteg.py
@@ -1,4 +1,6 @@
 import cv2
+import numpy as np
+from bitarray import bitarray
 
 
 # JSteg最早在JPEG图像中进行隠写的方法之一, 但该方法会使得DCT系数直方图有明显的变化 [使得DCT系数中(2i, 2i+1)的频率趋于一致]
@@ -10,11 +12,26 @@ import cv2
 # steganography 隠写
 
 
+def constant_pad_8_8(image, pad_val=0):
+    assert image.ndim == 2 or image.ndim == 3
+
+    h, w = image.shape[:2]
+    h_pad = 0 if h % 8 == 0 else 8 - h % 8
+    w_pad = 0 if w % 8 == 0 else 8 - w % 8
+
+    padded_image = cv2.copyMakeBorder(image, 0, h_pad, 0, w_pad, cv2.BORDER_CONSTANT, pad_val)
+    return padded_image
+
+
 def hide_the_text(image, text):
+    # 将文本转化成位数组
+    text_bit_array = bitarray(endian='big').frombytes(text.encode("UTF-8"))
+
     pass
 
 
 def reveal_the_text(image):
+    # text_bit_array.tobytes()
     pass
 
 
