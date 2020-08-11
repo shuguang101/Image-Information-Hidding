78fb794ec4106db3eb358e2ca9c14c5158ec48da
shuguang101 <1511013670@qq.com>
2020-08-11 11:10:48 +0800

    TODO 转换颜色空间

--- JSteg.py
+++ JSteg.py
@@ -13,28 +13,39 @@ from jpeg_table import light_table
 # steganography 隠写
 
 
-def insert_one_bit(sub_img, bit_data):
-    is_inserted = False
+def insert_one_bit(dct_q, bit_data, n=2, use_dc=True):
     # 将秘密信息位嵌入 '绝对值非0,1的系数' 中
-    for i in range(0, 8, 1):
-        for j in range(0, 8, 1):
-            if not is_inserted and sub_img[i, j] != 0 and abs(sub_img[i, j]) != 1:
-                sign = -1 if sub_img[i, j] < 0 else 1
-                print('---------------', bit_data, sub_img.dtype)
-                print(sub_img[i, j])
-                sub_img[i, j] = ((abs(sub_img[i, j]) & 0xfffffffe) | bit_data) * sign
-                print(sub_img[i, j])
-                is_inserted = True
-    return is_inserted
+    for i in range(0, n, 1):
+        for j in range(0, n, 1):
+            is_dc = i == 0 and j == 0
+            if dct_q[i, j] != 0 and abs(dct_q[i, j]) != 1 and (use_dc or not is_dc):
+                sign = -1 if dct_q[i, j] < 0 else 1
+                dct_q[i, j] = ((abs(dct_q[i, j]) & 0xfffffffe) | bit_data) * sign
+                return True
+    return False
 
 
-def hide_the_text(image, text):
+def extract_one_bit(dct_q, n=2, use_dc=True):
+    # 提取秘密信息位
+    for i in range(0, n, 1):
+        for j in range(0, n, 1):
+            is_dc = i == 0 and j == 0
+            if dct_q[i, j] != 0 and abs(dct_q[i, j]) != 1 and (use_dc or not is_dc):
+                return True, abs(dct_q[i, j]) & 0x00000001
+    return False, -1
+
+
+def hide_the_text(image, secret_data, mul=3):
+    # 调整量化表
+    inner_light_table = np.around(light_table * mul)
+
     # 3通道 float32图像
     assert image.ndim == 3 and (image.dtype == np.float32 or image.dtype == np.float64)
+    assert isinstance(secret_data, bytes)
 
     # 将文本转化成位数组
-    text_bit_array = bitarray(endian='big')
-    text_bit_array.frombytes(text.encode("UTF-8"))
+    text_bit_array = bitarray(endian='little')
+    text_bit_array.frombytes(secret_data)
     text_bit_array = np.array(text_bit_array.tolist()).astype(np.int)
     insert_bit_data_index = 0
 
@@ -50,9 +61,9 @@ def hide_the_text(image, text):
             img_dct_g = cv2.dct(image[hs:hs + 8, ws:ws + 8, 1] - 127.5)
             img_dct_r = cv2.dct(image[hs:hs + 8, ws:ws + 8, 2] - 127.5)
             # 使用亮度亮度量化表进行量化
-            img_dct_bq = np.around(img_dct_b / light_table).astype(np.int)
-            img_dct_gq = np.around(img_dct_g / light_table).astype(np.int)
-            img_dct_rq = np.around(img_dct_r / light_table).astype(np.int)
+            img_dct_bq = np.around(img_dct_b / inner_light_table).astype(np.int)
+            img_dct_gq = np.around(img_dct_g / inner_light_table).astype(np.int)
+            img_dct_rq = np.around(img_dct_r / inner_light_table).astype(np.int)
 
             # 将秘密信息位嵌入 '绝对值非0,1的系数' 中
             if insert_bit_data_index < text_bit_array.shape[0]:
@@ -63,58 +74,79 @@ def hide_the_text(image, text):
                 insert_bit_data_index += insert_one_bit(img_dct_rq, text_bit_array[insert_bit_data_index])
 
             # 反量化, 反dct变换, 恢复图像
-            steganography_image[hs:hs + 8, ws:ws + 8, 0] = 127.5 + cv2.idct(img_dct_bq * light_table * 1.0)
-            steganography_image[hs:hs + 8, ws:ws + 8, 1] = 127.5 + cv2.idct(img_dct_gq * light_table * 1.0)
-            steganography_image[hs:hs + 8, ws:ws + 8, 2] = 127.5 + cv2.idct(img_dct_rq * light_table * 1.0)
+            steganography_image[hs:hs + 8, ws:ws + 8, 0] = 127.5 + cv2.idct(img_dct_bq * inner_light_table * 1.0)
+            steganography_image[hs:hs + 8, ws:ws + 8, 1] = 127.5 + cv2.idct(img_dct_gq * inner_light_table * 1.0)
+            steganography_image[hs:hs + 8, ws:ws + 8, 2] = 127.5 + cv2.idct(img_dct_rq * inner_light_table * 1.0)
 
     return steganography_image
 
 
-# def reveal_the_text(steganography_image, length=192):
-#     assert steganography_image.ndim == 2 or steganography_image.ndim == 3
-#     if steganography_image.ndim == 2:
-#         steganography_image = np.expand_dims(steganography_image, -1)
-#
-#     h, w, c = steganography_image.shape[:3]
-#     text_bit_array = bitarray(endian='big')
-#     index = 0
-#
-#     for cs in range(0, c, 1):
-#         for hs in range(0, h // 8 * 8, 8):
-#             for ws in range(0, w // 8 * 8, 8):
-#                 # 对 8x8 局部图像进行 dct 变换 [-128, 127]
-#                 img_dct = cv2.dct(steganography_image[hs:hs + 8, ws:ws + 8, cs] - 128.0)
-#                 # 使用亮度亮度量化表进行量化
-#                 img_dct_q = (img_dct / light_table).astype(np.int)
-#
-#                 # 将秘密信息位嵌入 '绝对值非0,1的系数' 中
-#                 for i in range(0, 8, 1):
-#                     for j in range(0, 8, 1):
-#                         if index < length \
-#                                 and (i != 0 and j != 0) \
-#                                 and img_dct_q[i, j] != 0 \
-#                                 and abs(img_dct_q[i, j]) != 1:
-#                             text_bit_array.append(img_dct_q[i, j] & 0x01)
-#                             index += 1
-#                             i = 8
-#                             j = 8
-#                             if index == 1:
-#                                 print(cs, hs, ws, i, j)
-#                                 print(img_dct_q)
-#
-#     print(np.array(text_bit_array.tolist()).astype(np.int))
-#     return text_bit_array.tobytes().decode("UTF-8")
+def reveal_the_text(steganography_image, data_len_in_bit, mul=3):
+    # 调整量化表
+    inner_light_table = np.around(light_table * mul)
+
+    # 3通道 float32图像
+    assert steganography_image.ndim == 3
+    assert steganography_image.dtype == np.float32 or steganography_image.dtype == np.float64
+    assert isinstance(data_len_in_bit, int)
+
+    bit_data_array = bitarray(endian='little')  # big little
+    extract_bit_data_index = 0
+
+    # 嵌入秘密信息
+    for hs in range(0, steganography_image.shape[0] // 8 * 8, 8 * 3):
+        for ws in range(0, steganography_image.shape[1] // 8 * 8, 8 * 3):
+            # 对 8x8 局部图像进行 dct 变换 [-127.5, 127.5]
+            img_dct_b = cv2.dct(steganography_image[hs:hs + 8, ws:ws + 8, 0] - 127.5)
+            img_dct_g = cv2.dct(steganography_image[hs:hs + 8, ws:ws + 8, 1] - 127.5)
+            img_dct_r = cv2.dct(steganography_image[hs:hs + 8, ws:ws + 8, 2] - 127.5)
+            # 使用亮度亮度量化表进行量化
+            img_dct_bq = np.around(img_dct_b / inner_light_table).astype(np.int)
+            img_dct_gq = np.around(img_dct_g / inner_light_table).astype(np.int)
+            img_dct_rq = np.around(img_dct_r / inner_light_table).astype(np.int)
+            # 提取秘密信息
+            if extract_bit_data_index < data_len_in_bit:
+                is_extracted, bit_data = extract_one_bit(img_dct_bq)
+                extract_bit_data_index += is_extracted
+                if is_extracted:
+                    bit_data_array.append(bit_data)
+            if extract_bit_data_index < data_len_in_bit:
+                is_extracted, bit_data = extract_one_bit(img_dct_gq)
+                extract_bit_data_index += is_extracted
+                if is_extracted:
+                    bit_data_array.append(bit_data)
+            if extract_bit_data_index < data_len_in_bit:
+                is_extracted, bit_data = extract_one_bit(img_dct_rq)
+                extract_bit_data_index += is_extracted
+                if is_extracted:
+                    bit_data_array.append(bit_data)
+
+    return bit_data_array.tobytes()
 
 
 if __name__ == "__main__":
     # 读取图像
-    img = cv2.imread("images/corel_bavarian_couple.jpg")[:, :, :3]
+    raw_img = cv2.imread("images/corel_bavarian_couple.jpg")[:, :, :3]
 
-    # print((img * 1.0).dtype)
+    # 秘密信息
+    raw_data = "you can't seen this text".encode("UTF-8")
+
     # 嵌入字符串
-    steganography_img = hide_the_text(img * 1.0, "you can't seen this text")
-    cv2.imwrite("steganography_img.jpg", steganography_img)
-    # #
-    # # print(reveal_the_text(steganography_img, 192))
-    #
-    # cv2.waitKey()
+    steganography_img = hide_the_text(raw_img * 1.0, raw_data, 17)
+    # 0-100
+    cv2.imwrite("steganography_img.jpg", steganography_img, [cv2.IMWRITE_JPEG_QUALITY, 20])
+
+    # 读取隠写图像
+    steganography_img = cv2.imread("steganography_img.jpg")[:, :, :3]
+    # 提取秘密信息
+    extracted_data = reveal_the_text(steganography_img * 1.0, len(raw_data) * 8, 17)
+
+    # 打印提取到的信息
+    print(raw_data == extracted_data)
+    print("r: ", raw_data.decode("UTF-8"))
+    print("e: ", extracted_data.decode("UTF-8"))
+
+    # 显示图像
+    cv2.imshow("raw_img", raw_img)
+    cv2.imshow("steganography_img", steganography_img)
+    cv2.waitKey()
